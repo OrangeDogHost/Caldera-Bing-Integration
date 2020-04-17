eb70c62aad555c47c0ec61cd0d04e8db2ebfb714
Zack Palmer <zacnoo@gmail.com>
2020-04-17 12:25:28 +1200

    Reverted previous commit, changed js directory stuff around.

--- caldera_bing_integration.php
+++ caldera_bing_integration.php
@@ -6,9 +6,9 @@ Version: 0.1-alpha2
 Author: Zack Palmer (zacnoo)
 */
 
-add_action( 'wp_enqueue_script', 'caldera_bing_loader' );
+add_action( 'wp_enqueue_scripts', 'caldera_bing_loader' );
 function caldera_bing_loader(){
-  wp_enqueue_script( 'caldera_bing_integration', plugin_dir_url( __FILE__ ) . '/js/caldera_bing_integration.js', array( 'jquery' ) );
+  wp_enqueue_script( 'caldera_bing_integration.js', plugin_dir_url( __FILE__ ) . '/js', array( 'jquery' ) );
 }
 
 ?>
