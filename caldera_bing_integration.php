a9079d5488547a5ea5fcef342a27fb0bd38d7226
Zack Palmer <zacnoo@gmail.com>
2020-04-17 12:53:16 +1200

    Added jQuery to load first. Because for some reason even thought it's set as a dependency of our plugin, it doesn't seem to load first..

--- caldera_bing_integration.php
+++ caldera_bing_integration.php
@@ -2,13 +2,14 @@
 /**
 Plugin Name: Caldera Forms Bing Integration
 Description: Adds Bing Maps API to caldera.
-Version: 0.1-alpha2
+Version: 0.1-alpha3
 Author: Zack Palmer (zacnoo)
 */
 
 add_action( 'wp_enqueue_scripts', 'caldera_bing_loader' );
 function caldera_bing_loader(){
-  wp_enqueue_script( 'caldera_bing_integration', plugin_dir_url( __FILE__ ) . '/js/caldera_bing_integration.js', array( 'jquery' ) );
+  wp_enqueue_script("jquery");
+  wp_enqueue_script( 'caldera_bing_integration', plugin_dir_url( __FILE__ ) . '/js/caldera_bing_integration.js', array('jquery') );
 }
 
 ?>
