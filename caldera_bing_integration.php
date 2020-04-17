a845e7b894bbf7f275124ee7c7ae29af4190b543
Zack Palmer <zacnoo@gmail.com>
2020-04-17 12:23:05 +1200

    Changed (incorrect?) action name to match the function name.

--- caldera_bing_integration.php
+++ caldera_bing_integration.php
@@ -6,7 +6,7 @@ Version: 0.1-alpha2
 Author: Zack Palmer (zacnoo)
 */
 
-add_action( 'wp_enqueue_scripts', 'caldera_bing_loader' );
+add_action( 'wp_enqueue_script', 'caldera_bing_loader' );
 function caldera_bing_loader(){
   wp_enqueue_script( 'caldera_bing_integration', plugin_dir_url( __FILE__ ) . '/js/caldera_bing_integration.js', array( 'jquery' ) );
 }
